e44c72f567be714071637bb22fce490038a42047
Kalai <kalaichandran369@gmail.com>
2021-12-07 13:47:36 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,55 @@
+import img1 from './img/img1.jpg';
+import img2 from './img/img2.jpg';
+import img3 from './img/img3.jpg';
+import img4 from './img/img4.jpg';
+import img5 from './img/img5.JPG';
+import img6 from './img/img6.jpg';
+
+const data = {
+    cardData:[
+        {
+            id:1,
+            img:img1,
+            title:'blog 1',
+            desc: 'desc 1'
+        },
+        {
+            id:2,
+            img:img2,
+            title:'blog 2',
+            desc: 'desc 2'
+        },
+        {
+            id:3,
+            img:img3,
+            title:'blog 3',
+            desc: 'desc 3'
+        },
+        {
+            id:4,
+            img:img4,
+            title:'blog 4',
+            desc: 'desc 4'
+        },
+        {
+            id:5,
+            img:img5,
+            title:'blog 5',
+            desc: 'desc 5'
+        },
+        {
+            id:6,
+            img:img6,
+            title:'blog 6',
+            desc: 'desc 6'
+        },
+        {
+            id:7,
+            img:img4,
+            title:'blog 7',
+            desc: 'desc 7'
+        }
+    ]
+}
+
+export default data;
